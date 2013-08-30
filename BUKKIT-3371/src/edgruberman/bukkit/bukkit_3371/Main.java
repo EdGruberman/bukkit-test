af89f3a0981377534c160874f1e8864c1ace4c7e
EdGruberman <ed@rjump.com>
2013-08-30 10:11:11 -0700

    BUKKIT-3371; create and change commands

--- BUKKIT-3371/src/edgruberman/bukkit/bukkit_3371/Main.java
+++ BUKKIT-3371/src/edgruberman/bukkit/bukkit_3371/Main.java
@@ -1,5 +1,7 @@
 package edgruberman.bukkit.bukkit_3371;
 
+import java.util.Collection;
+
 import org.bukkit.Location;
 import org.bukkit.block.BlockFace;
 import org.bukkit.command.Command;
@@ -16,12 +18,33 @@ public final class Main extends JavaPlugin {
         final Location target = player.getTargetBlock(null, 100).getLocation();
         final BlockFace direction = BlockFace.valueOf(args[0].toUpperCase());
 
-        this.getServer().broadcastMessage(target.getBlock().getType().name() + " " + direction.name() + " " + target.toString());
+        this.getServer().broadcastMessage("target: " + target.getBlock().getType().name() + " " + direction.name() + " " + target.toString());
 
-        final ItemFrame frame = target.getWorld().spawn(target, ItemFrame.class);
-        frame.setFacingDirection(direction, true);
+        if (command.getName().equals("create")) {
+            this.create(target, direction);
+
+        } else if (command.getName().equals("change")) {
+            this.change(target, direction);
+        }
 
         return true;
+    }
+
+    private void create(final Location target, final BlockFace direction) {
+        final ItemFrame frame = target.getWorld().spawn(target, ItemFrame.class);
+        this.getServer().broadcastMessage("create frame: " + frame.getLocation().getBlock().getType().name() + " " + frame.getFacing().name() + " " + frame.getLocation().toString());
+        frame.setFacingDirection(direction, true);
+    }
+
+    private void change(final Location target, final BlockFace direction) {
+        final Collection<ItemFrame> frames = target.getWorld().getEntitiesByClass(ItemFrame.class);
+        this.getServer().broadcastMessage("frames found: " + frames.size());
+        for (final ItemFrame frame : frames) {
+            if (frame.getLocation().distance(target) > 2) continue;
+            this.getServer().broadcastMessage("change frame: " + frame.getLocation().getBlock().getType().name() + " " + frame.getFacing().name() + " " + frame.getLocation().toString());
+            frame.setFacingDirection(direction);
+            break;
+        }
     }
 
 }
